bf39d09417287f33e5997ad9757da62d001e1b6e
Chris Vishoot <chrisvishoot@Chriss-MacBook-Pro.local>
2026-03-01 11:45:28 -0800

    Updated the code base to be able to be deployed

---
+++ golang/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Cross-compile for Linux amd64 (for Docker/Kubernetes)
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main .
+
+# Build Docker image
+docker build -t golang-service:v2 .
+
+# Load into kind cluster (uncomment if using kind)
+#kind load docker-image golang-service:v1
+
